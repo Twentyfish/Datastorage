0f461a19dfa4728904429078903efc96d9bab305
Twentyfish <sebastiaan@dezeeuw.be>
2024-12-28 22:44:51 +0100

    Update and rename leaderstats to leaderstats.lua

--- leaderstats.lua
+++ leaderstats.lua
@@ -47,3 +47,41 @@ Players.PlayerAdded:Connect(function(plr)
 	local HumanoidRootPart = plr.Character:WaitForChild("HumanoidRootPart")
 	HumanoidRootPart.CFrame =  Checkpoints[Stage.Value].SpawnLocation.CFrame
 end)
+
+
+-- Checkpoint script that changes them below;
+
+local Checkpoint = script.Parent
+local StageNumber = Checkpoint.Name
+local CheckpointTouched = false
+local Players = game:GetService("Players")
+local DataStoreService = game:GetService("DataStoreService")
+local StageData = DataStoreService:GetDataStore("PlayerStage")
+local CoinsData = DataStoreService:GetDataStore("PlayerCoins")
+local CoinAddAmount = script.Parent.AmountOfCoins.Value
+
+Checkpoint.Touched:Connect(function(plr)
+	if plr.Parent:IsA("Accessory") then
+	else
+		local character = plr.Parent
+		local humanoid = character:WaitForChild("Humanoid")
+		local TouchPlayer = Players:GetPlayerFromCharacter(character)
+		local PlayerStage = Players:GetPlayerFromCharacter(character).leaderstats.Stage
+		local PlayerCoins = Players:GetPlayerFromCharacter(character).Coins
+
+		if humanoid then
+			if CheckpointTouched == false and humanoid:GetState() ~= Enum.HumanoidStateType.Dead then
+				if PlayerStage.Value < tonumber(StageNumber) then
+					TouchPlayer.RespawnLocation = script.Parent.SpawnLocation
+					CheckpointTouched = true
+					PlayerStage.Value = StageNumber
+					PlayerCoins.Value = PlayerCoins.Value + CoinAddAmount
+					print(PlayerStage.Value)
+					CoinsData:SetAsync(TouchPlayer.UserId, PlayerCoins.Value)
+					StageData:SetAsync(TouchPlayer.UserId, PlayerStage.Value)
+					Players:GetPlayerFromCharacter(character).PlayerGui.ScreenGui.Frame.TextLabel.Text = PlayerCoins.Value
+				end
+			end
+		end
+	end
+end)
